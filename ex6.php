49376f55b7ed42749a67eadb5900532343b05219
Swethasarathy <40590670+Swethasarathy@users.noreply.github.com>
2018-08-06 12:30:40 +0530

    Add files via upload

---
+++ ex6.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title></title>
+</head>
+<body>
+<form action="ex6ii.php" method="post">
+<br>Name: <input type="text" name="name"></br>
+<br>E-mail ID: <input type="text" name="mail"></br>
+<br>Address : <input type="text" name="addr"></br>
+<br><input type="submit" name="Submit"></br>
+</form>
+<?php
+// put your code here
+?>
+</body>
+</html>
+
